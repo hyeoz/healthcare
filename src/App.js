24760ed3dd64a872d693b0fd61475c5fd277c22d
ideal <ideal@MacBook-Pro.local>
2022-04-10 01:32:08 +0900

    chore: reactstrap 라이브러리 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './styles/App.css';
-
+import Main from './views/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
